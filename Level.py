a809ae3fd2946487431cab4d9983d5708988d36f
The-Frige <21050516+The-Frige@users.noreply.github.com>
2023-08-08 16:45:47 -0700

    Modified the level file to accept player objects so the user can move the player.

--- Level.py
+++ Level.py
@@ -11,7 +11,13 @@ class Level:
         json_file = json.load(open('Test.json', 'r', encoding="utf-8"))
         for object in json_file["objects"]:
             temp_object = Object(object.get("object_name"),object.get("object_width"), object.get("object_height"), object.get("object_x"), object.get("object_y"), object.get("image_path"))
-            self.Objects.add(temp_object)
+            if (temp_object.name == "player"):
+                #player_object = Player(object.get("object_name"),object.get("object_width"), object.get("object_height"), object.get("object_x"), object.get("object_y"), object.get("image_path"))
+                #self.Objects.add(player_object)
+                #the commented out sections are for when the player class gets implemented. It's supposed to follow the same format as the object class since it inherits from it.
+                pass
+            else:
+                self.Objects.add(temp_object)
        
     def add_obj(self, object):
         self.Objects.add(object)
