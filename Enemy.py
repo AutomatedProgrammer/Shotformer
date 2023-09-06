2f0f5057aa42f6b4828291e35bbd7980679931e4
The-Frige <21050516+The-Frige@users.noreply.github.com>
2023-09-05 19:19:50 -0700

    Added in enemy gravity and player jumping/gravity. Still needs bugfixing.

--- Enemy.py
+++ Enemy.py
@@ -1,9 +1,20 @@
+import pygame
+from Object import Object
+
+gravity = 5
+
 class Enemy(Object):
-    def init(self, name, width, height, x, y, image_path):
-        super(Enemy, self).init(name, width, height, x, y, image_path)
+
+    def __init__(self, name, width, height, x, y, image_path):
+        super(Enemy, self).__init__(name, width, height, x, y, image_path)
 
     def update(self):
-        pass
+        if self.rect.centery < 340:
+            self.move_y(gravity)
+        #self.move_y(gravity)
+        #self.rect.centery = self.y
+        self.rect.centerx = self.x
+        self.rect.centery = self.y
 
     
 # There is probably a much better way of doing this
--- Player.py
+++ Player.py
@@ -5,16 +5,50 @@ import random
 from Object import Object
 
 PL_V = 3  # player velocity, had to include this here
+gravity = 5
+jump_height = 100
 
 class Player(Object):
 
     def __init__(self, name, width, height, x, y, image_path):
         super(Player, self).__init__(name, width, height, x,y, image_path)
         self.in_middle = False
+        self.jumping = False
 
     def update(self):
         key = pygame.key.get_pressed()
 
+        #Jumping movement. I think to make it smoother we only set jumping to false after the key's been held down for a little bit.
+        if key[pygame.K_UP] and key[pygame.K_RIGHT] and self.jumping == False:
+            self.jumping = True
+            self.move_y(-jump_height)
+            if self.rect.centerx > 380:
+                self.in_middle = True
+
+            elif self.in_middle == False: 
+                self.move_x(PL_V)
+                
+        elif key[pygame.K_UP] and key[pygame.K_LEFT] and self.jumping == False:
+            self.jumping = True
+            self.move_y(-jump_height)
+            if self.rect.centerx > 0:
+                self.move_x(-PL_V)
+                self.in_middle = False
+
+        #Jumping just up
+        elif key[pygame.K_UP] and self.jumping == False:
+            self.jumping = True
+            self.move_y(-jump_height)
+
+        #Gravity
+        if self.rect.centery < 340:
+            self.move_y(gravity)
+            self.rect.centery = self.y
+            if self.rect.centery == 340:
+                self.jumping = False
+                
+
+        #Regular movement
         if key[pygame.K_LEFT]:
             if self.rect.centerx > 0:
                 self.move_x(-PL_V)
@@ -27,7 +61,12 @@ class Player(Object):
 
             elif self.in_middle == False: 
                 self.move_x(PL_V)
-                
+
+        
+
+        
+
+        
         self.rect.centerx = self.x
         self.rect.centery = self.y
 
