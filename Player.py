5a585e0c33c6f2299b8f77fa79147732c123ac32
The-Frige <21050516+The-Frige@users.noreply.github.com>
2023-09-10 12:11:53 -0700

    Fixed jumping bugs. Now can control how high you jump.

--- Player.py
+++ Player.py
@@ -6,7 +6,7 @@ from Object import Object
 
 PL_V = 3  # player velocity, had to include this here
 gravity = 5
-jump_height = 100
+jump_height = 10
 
 class Player(Object):
 
@@ -14,14 +14,18 @@ class Player(Object):
         super(Player, self).__init__(name, width, height, x,y, image_path)
         self.in_middle = False
         self.jumping = False
+        self.air_time = 0
 
     def update(self):
         key = pygame.key.get_pressed()
 
         #Jumping movement. I think to make it smoother we only set jumping to false after the key's been held down for a little bit.
         if key[pygame.K_UP] and key[pygame.K_RIGHT] and self.jumping == False:
-            self.jumping = True
+            if self.air_time > 50:
+                self.jumping = True
+                self.air_time = 0
             self.move_y(-jump_height)
+            self.air_time += 1
             if self.rect.centerx > 380:
                 self.in_middle = True
 
@@ -29,23 +33,31 @@ class Player(Object):
                 self.move_x(PL_V)
                 
         elif key[pygame.K_UP] and key[pygame.K_LEFT] and self.jumping == False:
-            self.jumping = True
+            if self.air_time > 50:
+                self.jumping = True
+                self.air_time = 0
             self.move_y(-jump_height)
+            self.air_time += 1
             if self.rect.centerx > 0:
                 self.move_x(-PL_V)
                 self.in_middle = False
 
         #Jumping just up
         elif key[pygame.K_UP] and self.jumping == False:
-            self.jumping = True
+            if self.air_time > 50:
+                self.jumping = True
+                self.air_time = 0
             self.move_y(-jump_height)
+            self.air_time += 1
 
         #Gravity
         if self.rect.centery < 340:
             self.move_y(gravity)
             self.rect.centery = self.y
+            self.air_time += 1
             if self.rect.centery == 340:
                 self.jumping = False
+                self.air_time = 0
                 
 
         #Regular movement
